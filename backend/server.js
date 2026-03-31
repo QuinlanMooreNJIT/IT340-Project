14ec0327970153f1c6e6b6e168d6dd740233a1d6
quinlanMoore <qem@njit.edu>
2026-03-30 23:58:41 -0400

    new unbuntu fixes on backend

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,9 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 
 const app = express();
+app.use(cors());
+app.use(express.json());
+
 const port = 3000;
 
 const mongoURI = "mongodb://192.168.75.137/27017/testdb";
@@ -11,8 +14,12 @@ mongoose.connect(mongoURI)
     .then(() => console.log("Conected to MongoDB"))
     .catch(err => console.log("MongoDB Connection error:", err));
 
-app.use(cors());
 
 app.get('/', (req, res) => res.send('Backend is running!'));
+
+app.get('/users', async (req, res) => {
+	const users = await mongoose.connection.db.collection("users").find().toArray();
+	res.json(users);
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
