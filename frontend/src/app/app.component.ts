75d0838e0937e1a81423b63e4356569e630fc301
Quinlan <qem@njit.edu>
2026-03-31 02:28:02 -0400

    Frontend commincating with Mongo Server, successfully, and shows proper html, but links on page are still nonfunctional

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { ApiService } from './api.service';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  template: '<h1>{{ message }}</h1>'
+  imports: [RouterModule],
+  templateUrl: './app.html',
 })
-export class AppComponent implements OnInit {
-  message = 'Loading...';
-  constructor(private api: ApiService) {}
-  ngOnInit(): void {
-    console.log('AppComponent initialized');
-    this.api.getMessage().subscribe(
-    data => {console.log('Data Recieved;', data); this.message = data; },
-    err => { console.error('Error fetching data', err); this.message = 'Error'; }
-    );
-  }
-}
+export class AppComponent {}
