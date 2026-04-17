049e76615c5a6ff77b9d192cef0abd958e2c27d4
quinlanMoore <qem@njit.edu>
2026-04-17 16:27:15 -0400

    added hashing check inside backend model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
     username: {
@@ -11,6 +12,16 @@ const userSchema = new mongoose.Schema({
         required: true
     }
 });
+
+userSchema.pre("save", async function(next){
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+userSchema.methods.comparePassword = function(candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+};
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
