521be88cfe3c183e3d469c80ae6e8dbef8a52718
quinlanMoore <qem@njit.edu>
2026-04-29 20:19:47 -0400

    created listing schema

---
+++ backend/models/Listing.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+Const ListingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    
+    description: {
+        type: String,
+        required: true
+    },
+    
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    
+    category: {
+        type: String,
+        required: true,
+        enum: ['instrument', 'service', 'equipment', 'other']
+    },
+    
+    user: {
+        type: mongoose.Schema.Types.ObjectID,
+        ref: 'User',
+        required: true
+    },
+    
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+}),
+
+module.exports = mongoose.model('Listing', ListingSchema)
