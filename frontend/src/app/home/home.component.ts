7d75e55ba93a32555b757d76c23ddfa070a8f54b
Quinlan <qem@njit.edu>
2026-04-29 21:54:46 -0400

    added form to submit listings

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ListingService } from '../services/listing.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html'
 })
@@ -15,6 +16,13 @@ import { ListingService } from '../services/listing.service';
 export class HomeComponent implements OnInit {
 
   listings: any[] = [];
+  
+  newListing = {
+    title: '',
+    descrption: '',
+    price: null,
+    category: 'instrument'
+  };
 
   constructor(
     public auth: AuthService, 
@@ -33,6 +41,24 @@ export class HomeComponent implements OnInit {
       },
       error: (err) => {
         console.error('Error loading listings', err);
+      }
+    });
+  }
+  
+  createListing() {
+    this.listingService.createListing(this.newListing).subscribe({
+      next: () => {
+        this.newListing = {
+        title: '',
+        description: '',
+        price: null,
+        category: 'instrument'
+        };
+        
+        this.loadListings();
+      },
+      error: (err) => {
+        console.error('Error creating listing', err);
       }
     });
   }
