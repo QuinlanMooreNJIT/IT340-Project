b2a1f1449ccb32f010fc7556ab4f32e7c202c8c1
Quinlan <qem@njit.edu>
2026-05-03 20:24:08 -0400

    implemented listings component to app routes

---
+++ frontend/src/app/listing-detail/listing-detail.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listing-detail',
+  imports: [],
+  templateUrl: './listing-detail.html',
+  styleUrl: './listing-detail.css',
+})
+export class ListingDetail {}
