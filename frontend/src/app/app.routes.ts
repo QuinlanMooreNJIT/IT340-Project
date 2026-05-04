7469c25099a61dc57ee7b6a90c5e507b5779f03a
Quinlan <qem@njit.edu>
2026-05-03 20:39:51 -0400

    debugging phase 2

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { ListingDetailComponent } from './listing-detail/listing-detail.component';
+import { ListingDetail } from './listing-detail/listing-detail';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'listing/:id', component: ListingDetailComponent },
+  { path: 'listing/:id', component: ListingDetail },
   { path: '**', redirectTo: '' },
 ];
