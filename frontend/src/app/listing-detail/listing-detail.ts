acc7493a706d736a30ca33f8be14797fd9eb2a5d
Quinlan <qem@njit.edu>
2026-05-03 21:39:58 -0400

    finalizing/debugging phase 2

--- frontend/src/app/listing-detail/listing-detail.ts
+++ frontend/src/app/listing-detail/listing-detail.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { ListingService } from '../services/listing.service';
 
 @Component({
   selector: 'app-listing-detail',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './listing-detail.html',
   styleUrl: './listing-detail.css',
 })
-export class ListingDetail {}
+export class ListingDetail implements OnInit {
+
+  listing: any;
+  
+  constructor(
+    private route: ActivatedRoute,
+    private listingService: ListingService
+  ) {}
+  
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+  
+    if (id) {
+      this.listingService.getListingById(id).subscribe({
+        next: (data) => {
+          this.listing = data;
+        },
+        error: (err) => {
+          console.error('Error loading listing:', err);
+        }
+      });
+    }
+  }
+}
