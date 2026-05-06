c8572587fb1816e016142ab9b7ee2637e0ab9aac
Quinlan <qem@njit.edu>
2026-05-06 18:19:46 -0400

    added comment service to frontend

---
+++ frontend/src/app/services/comment.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private apiUrl = 'http://192.168.10.20:3000/comments';
+  
+  constructor(private http: HttpClient) {}
+  
+  getComments(listingId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${listingId}`);
+  }
+  
+  addComment(listingId: string, content: string): Observable<any> {
+    const token = localStorage.getItem('token');
+    
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`
+    });
+    
+    return this.http.post(
+      this.apiUrl,
+      { listingId, content },
+      { headers }
+    );
+  }
+}
