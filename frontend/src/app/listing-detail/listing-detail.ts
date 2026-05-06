cb63514dab226c316e55e259a2b3c757c72eb5b2
Quinlan <qem@njit.edu>
2026-05-06 18:33:49 -0400

    added comment logic to listing-detail

--- frontend/src/app/listing-detail/listing-detail.ts
+++ frontend/src/app/listing-detail/listing-detail.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
 import { ListingService } from '../services/listing.service';
+import { CommentService } from '../services/comment.service';
 
 @Component({
   selector: 'app-listing-detail',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './listing-detail.html',
   styleUrl: './listing-detail.css',
 })
@@ -14,9 +17,13 @@ export class ListingDetail implements OnInit {
 
   listing: any;
   
+  comments: any[] = [];
+  newComment: string = '';
+  
   constructor(
     private route: ActivatedRoute,
     private listingService: ListingService,
+    private commentService: CommentService,
     private cdr: ChangeDetectorRef
   ) {}
   
@@ -29,11 +36,43 @@ export class ListingDetail implements OnInit {
       next: (data) => {
         this.listing = data;
         
+        this.loadComments();
+        
         this.cdr.detectChanges();
       },
       error: (err) => {
         console.error('Error loading listing:', err);
       }
     });    
+  }
+  
+  loadComments(): coid {
+    if(!this.listing?._id) return;
+    
+    this.commentService.getComments(this.listing._id).subscribe({
+      next: (data) => {
+        this.comments = data;
+      },
+      error: (err) => {
+        console.error('Error loading comments:', err);
+      }
+    });
+  }
+  
+  submitComment(): void {
+    if (!this.newComment.trim()) return;
+    
+    this.commentService.addComment(
+      this.listing._id,
+      this.newComment
+    ).subscribe({
+      next: () => {
+        this.newComment = '';
+        this.loadComments();
+      },
+      error: (err) => {
+        console.error('Error posting comments:', err);
+      }
+    });
   }
 }
