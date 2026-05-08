27eb69bc7dbd8eaaaf8000be120f44f5d29b02a6
Quinlan <qem@njit.edu>
2026-05-07 20:54:24 -0400

    build UI for cart component

--- frontend/src/app/cart/cart.ts
+++ frontend/src/app/cart/cart.ts
@@ -5,6 +5,7 @@ import { CartService } from '../services/cart.service';
 
 @Component({
   selector: 'app-cart',
+  standalone: true,
   imports: [CommonModule, RouterLink],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
@@ -30,6 +31,27 @@ export class CartComponent implements OnInit {
       },
       error: () => {
         this.loading = false;
+        alert('Failed to load cart')
+      }
+    });
+  }
+  
+  removeItem(listingId: string) {
+    this.cartService.removeFromCart(listingId).subscribe({
+      next: () => {
+        this.getCart();
+      }
+    });
+  }
+  
+  checkout() {
+    this.cartService.checkout().subscribe({
+      next: (res) => {
+        alert('Purchase successful!');
+        this.cartItems = [];
+      },
+      error: (err) => {
+        alert(err.error.message || 'Checkout failed');
       }
     });
   }
