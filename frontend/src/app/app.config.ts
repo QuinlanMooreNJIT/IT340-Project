1113df82bffd54f85704c022d7c2d1517ac66b23
Quinlan <qem@njit.edu>
2026-05-10 22:42:44 -0400

    fully implemented interceptors

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,34 +1,15 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { routes } from './app.routes';
-
-import { importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+
+import { routes } from './app.routes';
+import { authInterceptor } from './interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(
-    withInterceptors([
-      (req, next) => {
-        let token = null;
-        
-        if (typeof window !== 'undefined') {
-          token = localStorage.getItem('token');
-        }
-        
-        if (token) {
-          const cloned = req.clone ({
-            setHeaders: {
-              Authorization: `Bearer ${token}`
-            }
-          });
-          return next(cloned);
-        }
-        
-        return next(req);
-      }
-    ])
+    withInterceptors([authInterceptor])
     ),
     provideRouter(routes), 
     importProvidersFrom(FormsModule)
